c51aba777c6a9db4ca22bbbba44b90b19dead0f1
Ya <yaroslav.tlugachev@gmail.com>
2025-06-24 23:12:40 +0300

    host

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ async def fill_template(
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host = "127.0.0.1", port=10000, reload=True)
+    uvicorn.run("main:app", host = "0.0.0.0", port=10000, reload=True)
